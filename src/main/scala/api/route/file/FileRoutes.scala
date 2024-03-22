1a69956fd20861ba9a55b78b14b342283672fb14
unknown <pedro_gonsalves@hotmail.com>
2024-03-21 23:42:17 -0300

    refac: imports and constructions

--- src/main/scala/api/route/file/FileRoutes.scala
+++ src/main/scala/api/route/file/FileRoutes.scala
@@ -5,20 +5,21 @@ import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.directives.FileInfo
-import api.services.buckets.checkBucket
+import api.services
 import utils.configs
-
 import scala.util.Success
 import java.io.File
 import java.nio.file.{Files, Path}
-import api.services.file.{createRepository, deleteRepository}
+import api.services.objects
 
 class FileRoutes extends Directives {
-
-
+  
   def fileDestination(fileInfo: FileInfo)(implicit bucket:String): File =
     val repoName = fileInfo.fileName.split("\\.").toList.head
-    createRepository(bucket, repoName)
+    
+    val bucketOperations = new services.Buckets(bucket)
+    val repoOperations = new objects.Repository(bucketOperations, repoName)
+    repoOperations.create
     Files.createFile(Path.of(s"$bucketsPath\\$bucket\\$repoName\\${fileInfo.fileName}")).toFile
 
 
@@ -29,17 +30,16 @@ class FileRoutes extends Directives {
         post {
           parameter("bucket".as[String], "separator".as[String].optional) { (bucket, separator) =>
             implicit val bucketToSave: String = bucket
-            onComplete(checkBucket(bucket)) {
+            val bucketOperations = new services.Buckets(bucket)
+            onComplete(bucketOperations.check) {
               case Success(value) =>
+                
                 if (value)
                   storeUploadedFile("file", fileDestination) {
                     case (metadata, file) =>
-                      if (metadata.fileName.matches(".*\\.csv") && separator.orNull == null)
-                        deleteRepository(bucket, metadata.fileName.split("\\.").toList.head)
-                        complete(StatusCodes.UnprocessableContent, s"${metadata.fileName} needs a separator.")
-                      else
-                        complete(StatusCodes.OK)
+                      complete(StatusCodes.OK)
                   }
+                  
                 else
                   complete(StatusCodes.Conflict, s"Bucket $bucket not exists.")
             }
