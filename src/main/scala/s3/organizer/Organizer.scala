52e0b3e4e8c9e69c2d0b2227d55b354522f97d77
unknown <pedro_gonsalves@hotmail.com>
2024-04-01 23:36:58 -0300

    add: docstrings into all methods

--- src/main/scala/s3/organizer/Organizer.scala
+++ src/main/scala/s3/organizer/Organizer.scala
@@ -7,8 +7,25 @@ trait Organizer {
   
   val organizerPath:String
 
+  /**
+   * A method that materialize the organizer
+   *
+   * @return result of creation operation
+   */
   def create: Future[Unit]
+  
+  /**
+   * A method that checks to see if that organizer exists
+   *
+   * @return true for exist and false for non exists
+   */
   def check: Boolean
+  
+  /**
+   * A method that exclude all files and dirs inside of organizer
+   *
+   * @return result of delete operation
+   */
   def exclude: Future[Unit]
 
 }
