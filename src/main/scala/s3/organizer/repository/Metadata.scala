152e660d67a1e6a9d7aa201a16c81536e5729b16
unknown <pedro_gonsalves@hotmail.com>
2024-04-04 22:01:47 -0300

    refac: remove id field in models and uuid generators

--- src/main/scala/s3/organizer/repository/Metadata.scala
+++ src/main/scala/s3/organizer/repository/Metadata.scala
@@ -3,34 +3,32 @@ package s3.organizer.repository
 
 
 import s3.metadata
-import s3.organizer.bucket.{Bucket, listUUIDs}
+import s3.organizer.bucket.Bucket
 import utils.configs.bucketsPath
 import org.json4s.JsonAST.JObject
 import org.json4s.{JBool, JField, JInt, JString}
-import s3.utils.generateNewUUID
 import scala.concurrent.Future
 
 
 class Metadata(
                 bucket:Bucket,
-                repositoryName:String,
+                name:String,
                 versioned:Boolean
               ) extends metadata.Metadata:
 
-  override val metadataPath:String = s"$bucketsPath\\${bucket.bucketName}\\$repositoryName\\$metadataFileName"
+  override val metadataPath:String = s"$bucketsPath\\${bucket.name}\\$name\\$metadataFileName"
   
   import scala.concurrent.ExecutionContext.Implicits.global
 
   override def _content: Future[JObject] =
-    listUUIDs.map { fUuids =>
+    Future:
+        JObject(
+          "created_at" -> JString(java.time.LocalDateTime.now().toString),
+          "created_by" -> JString("admin"),
+          "objects" -> JInt(0),
+          "active" -> JBool(true),
+          "versioned" -> JBool(versioned)
+        )
 
-      JObject(
-        "id" -> JString(generateNewUUID(fUuids)),
-        "created_at" -> JString(java.time.LocalDateTime.now().toString),
-        "created_by" -> JString("admin"),
-        "objects" -> JInt(0),
-        "active" -> JBool(true),
-        "versioned" -> JBool(versioned)
-      )
-    }
+
 
