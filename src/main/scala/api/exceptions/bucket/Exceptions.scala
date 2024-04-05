66362083c4bc8c5787618ca434731f4e9858f87a
unknown <pedro_gonsalves@hotmail.com>
2024-04-04 22:18:26 -0300

    remove: uuidbucketnotexists exception

--- src/main/scala/api/exceptions/bucket/Exceptions.scala
+++ src/main/scala/api/exceptions/bucket/Exceptions.scala
@@ -3,4 +3,3 @@ package api.exceptions.bucket
 
 case class BucketNotExists(name:String) extends Exception
 case class BucketExists(name:String) extends Exception
-case class UUIdBucketNotExists(id:String) extends Exception
