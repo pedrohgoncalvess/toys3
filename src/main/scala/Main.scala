96b03a95843528593432a4f784dfb1ea5d934a37
unknown <pedro_gonsalves@hotmail.com>
2024-07-03 18:24:43 -0300

    fix: debug log in initialize environment

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,8 +11,10 @@ import api.controllers
 import database.migration.Migration.flyway
 import io.github.cdimascio.dotenv.Dotenv
 import database.operations.InteractUser.{createNewUser, existsAdmin}
+import java.nio.file.{Files, Paths}
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
+import scala.concurrent.duration.*
+import scala.util.{Failure, Success}
 
 
 private def initializeService: Future[Unit] =
@@ -21,23 +23,36 @@ private def initializeService: Future[Unit] =
 
   val dotenv = Dotenv.load
 
-  val envUsername = dotenv.get("TOY_S3_ROOT_USER")
-  val envPassword = dotenv.get("TOY_S3_ROOT_PASSWORD")
+  val s3Path = dotenv.get("DATA_PATH") match
+    case null => "./toys3"
+    case path => s"$path/toys3"
 
-  val adminUsername = envUsername match
-    case user => user
-    case null => "admin"
-
-  val adminPassword = envPassword match
-    case null => "admin"
-    case pass => pass
-
-  if (adminUsername == "admin" || adminPassword == "admin")
-    println("The system is using default credentials. Consider changing for added security.")
-
+  if (!Files.exists(Paths.get(s3Path)))
+      println(s"Created S3 data path in ${Paths.get(s3Path)}")
+      Files.createDirectories(Paths.get(s3Path))
+  else
+    println(s"S3 data path already exists in ${Paths.get(s3Path)}")
 
   existsAdmin.map( t =>
-    if (!t) createNewUser(user = adminUsername, password = adminPassword, admin = true, id_profile = Some(null))
+    if (!t) {
+
+      val envUsername = dotenv.get("TOY_S3_ROOT_USER") match
+        case null => "admin"
+        case user => user
+
+      val envPassword = dotenv.get("TOY_S3_ROOT_PASSWORD") match
+        case null => "admin"
+        case pass => pass
+
+      if (envUsername == "admin" || envPassword == "admin")
+        println("The system is using default credentials. Consider changing for added security.")
+
+      val creatingNewUser = createNewUser(user = envUsername, password = envPassword, admin = true, id_profile = Some(null))
+      creatingNewUser.onComplete{
+        case Success(_) => Future.successful
+        case Failure(exception) => throw exception
+      }
+    }
   )
 
 
@@ -53,11 +68,12 @@ private def initializeService: Future[Unit] =
   val bucket = new controllers.bucket.Router
   val file = new controllers.file.Router
   val repository = new controllers.repository.Router
+  val auth = new controllers.auth.Router
 
   implicit val system: ActorSystem[Any] = ActorSystem(Behaviors.empty, "my-system")
   implicit val executionContext: ExecutionContextExecutor = system.executionContext
 
-  val bindingFuture = Http().newServerAt("0.0.0.0", 8080).bind(Directives.concat(bucket.route, file.route, repository.route))
+  val bindingFuture = Http().newServerAt("0.0.0.0", 8080).bind(Directives.concat(bucket.route, file.route, repository.route, auth.route))
 
   println(s"Server online at port 8080.")
   StdIn.readLine()
