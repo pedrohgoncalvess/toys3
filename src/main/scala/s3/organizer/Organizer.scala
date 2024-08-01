22147087f234fd593ea0f06d71641d991326728b
unknown <pedro_gonsalves@hotmail.com>
2024-07-31 23:41:47 -0300

    add: adapte create method to accept external metadata and refac format of imports

--- src/main/scala/s3/organizer/Organizer.scala
+++ src/main/scala/s3/organizer/Organizer.scala
@@ -1,28 +1,32 @@
 package pedro.goncalves
 package s3.organizer
 
+
 import scala.concurrent.Future
+
+import org.json4s.JsonAST.JObject
+
 
 trait Organizer {
   
   val organizerPath:String
 
   /**
-   * A method that materialize the organizer
+   * A method that materialize the organizer.
    *
    * @return result of creation operation
    */
-  def create: Future[Unit]
+  def create(c: Option[JObject]): Future[Unit]
   
   /**
-   * A method that checks to see if that organizer exists
+   * A method that checks to see if that organizer exists.
    *
    * @return true for exist and false for non exists
    */
   def check: Boolean
   
   /**
-   * A method that exclude all files and dirs inside of organizer
+   * A method that exclude all files and dirs inside of organizer.
    *
    * @return result of delete operation
    */
