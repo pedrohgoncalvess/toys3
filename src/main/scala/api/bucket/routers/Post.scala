e3bac0435c58f5472fdb796cd00c90628d7ee967
unknown <pedro_gonsalves@hotmail.com>
2024-07-31 23:54:55 -0300

    add: new parameters to create bucket

--- src/main/scala/api/bucket/routers/Post.scala
+++ src/main/scala/api/bucket/routers/Post.scala
@@ -4,32 +4,41 @@ package api.bucket.routers
 
 import scala.util.{Failure, Success}
 
+import org.json4s.{JArray, JObject, JString}
 import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.model.StatusCodes
 
-
-import s3.organizer.bucket.Bucket
-import api.bucket.BucketJsonSupport
+import api.bucket.exceptions.BucketExists
 import api.auth.Service.endpointAuthenticator
+import s3.organizer.implementations.Bucket
+import api.bucket.models.{CreateBucket, PostJsonSupport}
 
 
-class Post extends Directives with BucketJsonSupport:
+class Post extends Directives with PostJsonSupport:
 
   val route: Route = authenticateOAuth2(realm = "secure site", endpointAuthenticator) { auth =>
     authorize(true) { // TODO: Implement authorization logic 
       post {
-        entity(as[api.bucket.Bucket]) { bucket =>
+        entity(as[CreateBucket]) { bucket =>
           val bucketOperations = Bucket(bucket.name)
 
           if (bucketOperations.check)
-            complete(StatusCodes.Conflict, s"Bucket ${bucket.name} already exists.")
+            throw BucketExists(bucket.name)
 
-          else
-            onComplete(bucketOperations.create) {
-              case Success(_) => complete(StatusCodes.OK)
-              case Failure(exception) => complete(StatusCodes.InternalServerError, exception.getMessage)
-            }
+          val jDescription = if bucket.description.orNull != null then JObject("description" -> JString(bucket.description.get)) else null
+          val jTags = if bucket.tags.orNull != null then JObject("tags" -> JArray(bucket.tags.get.map(i => JString(i)).toList)) else null
+
+          val externalMetadata: Option[JObject] =
+            if jDescription != null || jTags != null
+              then Some(jDescription.merge(jTags))
+            else
+              Some(null)
+
+          onComplete(bucketOperations.create(externalMetadata)) {
+            case Success(_) => complete(StatusCodes.OK)
+            case Failure(exception) => complete(StatusCodes.InternalServerError, exception.getMessage)
+          }
         }
       }
     }
