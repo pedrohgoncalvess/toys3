f769b0746933008fa27d6783987c9302e50b86d8
unknown <pedro_gonsalves@hotmail.com>
2024-07-31 23:46:34 -0300

    refac: generate token function and refac changing scope of valide token

--- src/main/scala/api/auth/Service.scala
+++ src/main/scala/api/auth/Service.scala
@@ -12,6 +12,7 @@ import pdi.jwt.{Jwt, JwtAlgorithm, JwtClaim}
 import akka.http.scaladsl.server.directives.Credentials
 
 import api.auth.exceptions.{ExpiredToken, NotValidToken}
+import database.models.User
 
 
 object Service:
@@ -19,23 +20,23 @@ object Service:
   private val dotenv = Dotenv.load
   private val jwtSecretKey = dotenv.get("SECRET_API_KEY")
 
-  def generateToken(username: String, admin: Boolean): String =
+  def generateToken(user:User): String =
     val now = Clock.systemUTC().instant().getEpochSecond
     val expiry = now + (5 * 3600000) / 1000
 
-    var claim = JwtClaim(s"""{"user":"$username", "admin":$admin}""")
+    var claim = JwtClaim(s"""{"user_id":"${user.id}", "admin":${user.admin}}""")
     claim = claim.expiresAt(expiry)
 
     Jwt.encode(claim, jwtSecretKey, JwtAlgorithm.HS256)
 
 
-  def validateToken(token: String): Option[String] =
+  private def validateToken(token: String): Option[String] =
       Jwt.decodeRaw(token, jwtSecretKey, Seq(JwtAlgorithm.HS256)) match
       case Success(decoded) =>
         val payload = Json.parse(decoded)
-        val username = (payload \ "user").as[String]
+        val userID = (payload \ "user_id").as[String]
         //val profileID = (payload \ "profile").as[Int]
-        Some(username)
+        Some(userID)
 
       case Failure(_: JwtExpirationException) => throw ExpiredToken()
       case Failure(_: JwtEmptyAlgorithmException) => throw NotValidToken()
