e83b8d9727696a8fd7083d2d9e8b0945d67b06d9
unknown <pedro_gonsalves@hotmail.com>
2024-08-01 00:00:10 -0300

    refac: change query parameter of name to id

--- src/main/scala/api/repository/routers/Get.scala
+++ src/main/scala/api/repository/routers/Get.scala
@@ -12,29 +12,27 @@ import api.repository.Service.jsonRepositories
 import api.auth.Service.endpointAuthenticator
 import api.repository.RepositoryJsonSupport
 import api.bucket.exceptions.BucketNotExists
-import utils.configs.projectPath
 
 
 class Get extends Directives with RepositoryJsonSupport:
-  
-  val rootDir: String = projectPath()
 
   val route: Route = authenticateOAuth2(realm = "secure site", endpointAuthenticator) { auth =>
     authorize(true) { // TODO: Implement authorization logic    
       get {
-          parameter("bucket-name".as[String]) { nameBucket =>
+          parameter("bucket-id".as[String]) { idBucket =>
             onComplete(jsonBuckets) {
               case Success(buckets) =>
-                val currentBucket = buckets.buckets.filter(_.name == nameBucket)
+                val currentBucket = buckets.buckets.filter(_.id == idBucket)
                 if (currentBucket.isEmpty)
-                  throw BucketNotExists(nameBucket)
+                  throw BucketNotExists(idBucket)
 
-                val repositories = jsonRepositories(currentBucket.head)
+                val repositories = jsonRepositories(Some(currentBucket.head))
 
                 onComplete(repositories) {
                   case Success(repos) => complete(StatusCodes.OK, repos)
                   case Failure(exception) => complete(StatusCodes.InternalServerError, exception.getMessage)
                 }
+              case Failure(exception) => complete(StatusCodes.InternalServerError, exception.getMessage)
             }
           }
         }
